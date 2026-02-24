698508a3f10daaf25ffdce146fc5a57acb5a0fd6
Jordy Montalvo <jordyjosephmontalvo@gmail.com>
2026-02-24 10:25:12 -0500

    Update link acceso docentes estudiantes

--- src/components/sections/FooterSection.vue
+++ src/components/sections/FooterSection.vue
@@ -105,9 +105,12 @@
           <div class="footer-column">
             <h4 class="nowrap">Acceso a portales</h4>
             <ul>
-              <li><a href="https://atl-sdcas01.sirsidynix.net/aiep_cas_saml/clientredirect?client_name=AIEP%20Students&service=https%3A%2F%2Fbibliotecas.aiep.cl%2Fclient%2Fes_CL%2Fdefault%2Fsearch%2Fcasrequest%3F" target="_blank" rel="noopener noreferrer">Estudiantes</a></li>
-              <li><a href="https://atl-sdcas01.sirsidynix.net/aiep_cas_saml/clientredirect?client_name=AIEP%20Students&service=https%3A%2F%2Fbibliotecas.aiep.cl%2Fclient%2Fes_CL%2Fdefault%2Fsearch%2Fcasrequest%3F" target="_blank" rel="noopener noreferrer">Docentes</a></li>
-              <li><a href="https://atl-sdcas01.sirsidynix.net/aiep_cas_saml/clientredirect?client_name=AIEP%20Staff&service=https%3A%2F%2Fbibliotecas.aiep.cl%2Fclient%2Fen_US%2Fdefault%2Fsearch%2Fcasrequest%3F" target="_blank" rel="noopener noreferrer">Colaboradores</a></li>
+              <li><a href="<https://aiep.ent.sirsidynix.net/client/es_CL/default/search/patronlogin/https:$002f$002faiep.ent.sirsidynix.net$002fclient$002fdefault$002fsearch$002faccount$003f>
+" target="_blank" rel="noopener noreferrer">Estudiantes</a></li>
+              <li><a href="<https://aiep.ent.sirsidynix.net/client/es_CL/default/search/patronlogin/https:$002f$002faiep.ent.sirsidynix.net$002fclient$002fdefault$002fsearch$002faccount$003f>
+" target="_blank" rel="noopener noreferrer">Docentes</a></li>
+              <li><a href="<https://aiep.ent.sirsidynix.net/client/es_CL/default/search/patronlogin/https:$002f$002faiep.ent.sirsidynix.net$002fclient$002fdefault$002fsearch$002faccount$003f>
+" target="_blank" rel="noopener noreferrer">Colaboradores</a></li>
               <li><a href="https://www.aiep.cl/titulados/" target="_blank" rel="noopener noreferrer">Titulados</a></li>
               <li><a href="#">Portal de pago</a></li>
             </ul>
--- src/components/sections/HeaderSection.vue
+++ src/components/sections/HeaderSection.vue
@@ -2,9 +2,12 @@
   <div class="top-bar-wrapper">
     <div class="top-bar-white" :class="{ 'hide-on-digitales': isLibrosDigitales }">
       <div class="top-bar-links">
-        <a href="https://atl-sdcas01.sirsidynix.net/aiep_cas_saml/login?service=https%3A%2F%2Fbibliotecas.aiep.cl%2Fclient%2Fes_CL%2Fdefault%2Fsearch%2Fcasrequest%3F" target="_blank" rel="noopener noreferrer" class="top-link">Estudiantes</a>
-        <a href="https://atl-sdcas01.sirsidynix.net/aiep_cas_saml/login?service=https%3A%2F%2Fbibliotecas.aiep.cl%2Fclient%2Fes_CL%2Fdefault%2Fsearch%2Fcasrequest%3F" target="_blank" rel="noopener noreferrer" class="top-link">Docentes</a>
-        <a href="https://atl-sdcas01.sirsidynix.net/aiep_cas_saml/login?service=https%3A%2F%2Fbibliotecas.aiep.cl%2Fclient%2Fes_CL%2Fdefault%2Fsearch%2Fcasrequest%3F" target="_blank" rel="noopener noreferrer" class="top-link">Colaboradores</a>
+        <a href="<https://aiep.ent.sirsidynix.net/client/es_CL/default/search/patronlogin/https:$002f$002faiep.ent.sirsidynix.net$002fclient$002fdefault$002fsearch$002faccount$003f>
+" target="_blank" rel="noopener noreferrer" class="top-link">Estudiantes</a>
+        <a href="<https://aiep.ent.sirsidynix.net/client/es_CL/default/search/patronlogin/https:$002f$002faiep.ent.sirsidynix.net$002fclient$002fdefault$002fsearch$002faccount$003f>
+" target="_blank" rel="noopener noreferrer" class="top-link">Docentes</a>
+        <a href="<https://aiep.ent.sirsidynix.net/client/es_CL/default/search/patronlogin/https:$002f$002faiep.ent.sirsidynix.net$002fclient$002fdefault$002fsearch$002faccount$003f>
+" target="_blank" rel="noopener noreferrer" class="top-link">Colaboradores</a>
       </div>
     </div>
   </div>
